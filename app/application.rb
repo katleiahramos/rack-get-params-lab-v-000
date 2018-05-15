157b7ef95cadfa7867fabe89be6de8c25aa6408f
Katleiah Ramos <katleiahramos@gmail.com>
2018-05-15 17:26:01 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -24,6 +24,7 @@ class Application
       end
     elsif req.path.match(/add/)
       search_term = req.params["q"]
+      binding.pry
       # if @@items.include?(search_term)
       #   @@cart << search_term
       #   resp.write "Added #{search_term}"
