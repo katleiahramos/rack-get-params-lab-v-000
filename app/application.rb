64b666b3b516b6112c7108c5c9c25aad0afaf543
Katleiah Ramos <katleiahramos@gmail.com>
2018-05-15 17:16:46 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -29,7 +29,7 @@ class Application
         @@cart << search_term
         resp.write "Added #{search_term}"
       else
-        resp.write "We don't have that item"
+        resp.write search_term
       end
     else
       resp.write "Path Not Found"
