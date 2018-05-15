e4eaaa77f9fa5b298a0c4818742db2d781d855ed
Katleiah Ramos <katleiahramos@gmail.com>
2018-05-15 17:04:59 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -16,7 +16,7 @@ class Application
     elsif req.path.match(/cart/)
       @@cart.each do |item|
         resp.write "#{item}\n"
-      end 
+      end
     else
       resp.write "Path Not Found"
     end
